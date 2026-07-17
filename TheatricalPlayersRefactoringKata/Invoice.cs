fa23f01b053d195a66a324c07de80445a75d2f8c
Gumega <gumega@gmail.com>
2026-07-17 13:46:17 -0300

    (refactor): adjusted text block from spaces to tabs

--- TheatricalPlayersRefactoringKata/Invoice.cs
+++ TheatricalPlayersRefactoringKata/Invoice.cs
@@ -4,16 +4,16 @@ namespace TheatricalPlayersRefactoringKata;
 
 public class Invoice
 {
-    private string _customer;
-    private List<Performance> _performances;
+	private string _customer;
+	private List<Performance> _performances;
 
-    public string Customer { get => _customer; set => _customer = value; }
-    public List<Performance> Performances { get => _performances; set => _performances = value; }
+	public string Customer { get => _customer; set => _customer = value; }
+	public List<Performance> Performances { get => _performances; set => _performances = value; }
 
-    public Invoice(string customer, List<Performance> performance)
-    {
-        this._customer = customer;
-        this._performances = performance;
-    }
+	public Invoice(string customer, List<Performance> performance)
+	{
+		this._customer = customer;
+		this._performances = performance;
+	}
 
 }
