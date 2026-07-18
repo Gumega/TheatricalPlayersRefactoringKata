fa29fab9c31002efdba8bc8994897328284c1c01
Gumega <gumega@gmail.com>
2026-07-17 22:48:37 -0300

    (refactor): simplified constructors

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -7,51 +7,53 @@ namespace TheatricalPlayersRefactoringKata.Tests;
 
 public class StatementPrinterTests
 {
-	[Fact]
+	/*[Fact]
 	public async Task TestStatementExampleLegacy()
 	{
-		var plays = new Dictionary<string, Play>();
-		plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-		plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-		plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+		Dictionary<string, Play> plays = new()
+        {
+            { "hamlet", new Play("Hamlet", 4024, "tragedy") },
+            { "as-like", new Play("As You Like It", 2670, "comedy") },
+            { "othello", new Play("Othello", 3560, "tragedy") }
+        };
 
-		Invoice invoice = new Invoice(
+		Invoice invoice = new(
 			"BigCo",
-			new List<Performance>
-			{
+            [
 				new Performance("hamlet", 55),
 				new Performance("as-like", 35),
 				new Performance("othello", 40),
-			}
+			]
 		);
 
 		var result = StatementPrinter.Print(invoice, plays);
 
 		await Verify(result);
-	}
+	}*/
 
 	[Fact]
 	public async Task TestTextStatementExample()
 	{
-		var plays = new Dictionary<string, Play>();
-		plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
-		plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
-		plays.Add("othello", new Play("Othello", 3560, "tragedy"));
-		plays.Add("henry-v", new Play("Henry V", 3227, "history"));
-		plays.Add("john", new Play("King John", 2648, "history"));
-		plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+        Dictionary<string, Play> plays = new()
+        {
+            { "hamlet", new Play("Hamlet", 4024, "tragedy") },
+            { "as-like", new Play("As You Like It", 2670, "comedy") },
+            { "othello", new Play("Othello", 3560, "tragedy") },
+            { "henry-v", new Play("Henry V", 3227, "history") },
+            { "john", new Play("King John", 2648, "history") },
+            { "richard-iii", new Play("Richard III", 3718, "history") }
+        };
 
-		Invoice invoice = new Invoice(
+		Invoice invoice = new(
 			"BigCo",
-			new List<Performance>
-			{
+            [
 				new Performance("hamlet", 55),
 				new Performance("as-like", 35),
 				new Performance("othello", 40),
 				new Performance("henry-v", 20),
 				new Performance("john", 39),
 				new Performance("henry-v", 20)
-			}
+			]
 		);
 
 		var result = StatementPrinter.Print(invoice, plays);
