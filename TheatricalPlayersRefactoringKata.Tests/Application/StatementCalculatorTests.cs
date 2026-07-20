d921af4e96231a08255decb10cf77df3b2a1a872
Gumega <gumega@gmail.com>
2026-07-20 01:55:14 -0300

    (refactor): added granular unit tests

---
+++ TheatricalPlayersRefactoringKata.Tests/Application/StatementCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using TheatricalPlayersRefactoringKata.Application;
+using TheatricalPlayersRefactoringKata.Domain.Model;
+using Xunit;
+
+namespace TheatricalPlayersRefactoringKata.Tests.Application
+{
+	public class StatementCalculatorTests
+	{
+		[Fact]
+		public void Calculate_ShouldConsolidateMultiplePerformancesCorrectly()
+		{
+			Invoice invoice = new("BigCo",
+			[
+				new Performance("hamlet", 55),
+				new Performance("as-like", 35)
+			]);
+
+			var plays = new Dictionary<string, Play>
+			{
+				{ "hamlet", new Play("Hamlet", 1000, "tragedy") },
+				{ "as-like", new Play("As You Like It", 1000, "comedy") }
+			};
+
+			var result = StatementBuilder.CreateModel(invoice, plays);
+
+			result.Customer.Should().Be("BigCo");
+			result.PerformanceResultList.Should().HaveCount(2);
+			result.TotalPrice.Should().BeGreaterThan(0);
+			result.TotalCredits.Should().BeGreaterThan(0);
+		}
+	}
+}
---
+++ TheatricalPlayersRefactoringKata.Tests/Infrastructure/XmlStatementFormatterTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using TheatricalPlayersRefactoringKata.Domain.Model;
+using TheatricalPlayersRefactoringKata.Infrastructure.Formatter;
+using Xunit;
+
+namespace TheatricalPlayersRefactoringKata.Tests.Infrastructure
+{
+	public class XmlStatementFormatterTests
+	{
+		[Fact]
+		public void Format_ShouldGenerateValidXmlMatchingExpectedSchema()
+		{
+			var formatter = new XMLStatementFormatter();
+			var data = new StatementData(
+				"BigCo",
+				[
+					new PerformanceResult("Hamlet", 650m, 25, 55)
+				],
+				650m,
+				25
+			);
+
+			string xmlResult = formatter.Format(data);
+
+			xmlResult.Should().Contain("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+			xmlResult.Should().Contain("<Customer>BigCo</Customer>");
+			xmlResult.Should().Contain("<AmountOwed>650</AmountOwed>");
+			xmlResult.Should().Contain("<EarnedCredits>25</EarnedCredits>");
+			xmlResult.Should().Contain("<Seats>55</Seats>");
+		}
+	}
+}
